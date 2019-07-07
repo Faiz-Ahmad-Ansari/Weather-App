2fd6d00f56547710f98817fb57c02c492268252e
Your Name <ansarifaiz32@gmail.com>
2019-07-07 12:53:06 +0530

    2.0 Added all parameters

--- hello.js
+++ hello.js
@@ -16,10 +16,45 @@ function renderHTML(data) {
     var city = document.getElementById("city");
     var cityName = data.name;
     city.innerHTML = " " + cityName;
-
+//Temperature
     var temp = document.getElementById("temp");
     var temperature = data.main.temp;
-    temp.innerHTML = " " + temperature;
+    var deg = "o";
+    temp.innerHTML = " " + temperature + deg.sup() + "C";
+//description
+    var desc = document.getElementById("description");
+    var description = data.weather[0].description;
+    desc.innerHTML = " "+ description;
+//Humidity
+    var humid = document.getElementById("humidity");
+    var humidity = data.main.humidity;
+    humid.innerHTML = " "+ humidity+"%";
+// Wind Speed
+    var wind = document.getElementById("wind");
+    var windSpeed =  data.wind.speed;
+    wind.innerHTML= " "+ windSpeed +" "+"m/s";
+// Clouds
+    var clds = document.getElementById("clouds");
+    var clouds = data.clouds.all;
+    clds.innerHTML = " "+clouds+"%";
+//Coord Long & Lat
+    var Coord1 = document.getElementById("coordLong");
+    var coordLong = data.coord.lon;
+    Coord1.innerHTML =" "+coordLong+" "+"/";
+    
+    var coord2 = document.getElementById("coordLat");
+    var coordLat = data.coord.lat;
+    coord2.innerHTML = " "+coordLat;
+
+    weatherIcon(data);
+    }
+    function weatherIcon(data) {
+        var imgElement = document.createElement("IMG");
+        var icon = data.weather[0].icon;
+        imgElement.setAttribute('src','https://openweathermap.org/img/wn/'+icon+'@2x.png');
+        imgElement.setAttribute('width','300');
+        imgElement.setAttribute('height','250');
+        document.body.appendChild(imgElement);
+    }
 
     }
-}
